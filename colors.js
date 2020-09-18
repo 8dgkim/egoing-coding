ca5b4bb342d2878ec646dab081e62e92fa23c0bf
DongGyu Kim <0dgkim@gmail.com>
2020-09-18 16:23:51 +0900

    jQuery (JavaScript)

--- colors.js
+++ colors.js
@@ -1,20 +1,23 @@
 var Links = {
     setColor: function (color) {
-        var links = document.querySelectorAll('a');
-        var i = 0;
+        // var links = document.querySelectorAll('a');
+        // var i = 0;
 
-        while (i < links.length) {
-            links[i].style.color = color;
-            i += 1;
-        }  
+        // while (i < links.length) {
+        //     links[i].style.color = color;
+        //     i += 1;
+        // } 
+        $('a').css('color', color);
     }
 }
 var Body = {
     setColor: function (color){
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor: function (color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 function themeControl(self) {
