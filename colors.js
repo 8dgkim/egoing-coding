46caf39951deac7503fe3cd2c381da6f39ca9e99
DongGyu Kim <0dgkim@gmail.com>
2020-09-18 15:43:37 +0900

    JavaScript

---
+++ colors.js
@@ -0,0 +1,36 @@
+var Links = {
+    setColor: function (color) {
+        var links = document.querySelectorAll('a');
+        var i = 0;
+
+        while (i < links.length) {
+            links[i].style.color = color;
+            i += 1;
+        }  
+    }
+}
+var Body = {
+    setColor: function (color){
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor: function (color) {
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
+function themeControl(self) {
+    var target = document.querySelector('body');
+
+    if (self.value === 'night') {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'day';
+        
+        Links.setColor('powderblue');           
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value = 'night';
+
+        Links.setColor('blue');
+    }
+}
