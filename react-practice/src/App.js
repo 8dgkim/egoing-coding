06268f75eb52efde774bea7af13a85e25eedb0c1
8dgkim <0dgkim@gmail.com>
2021-12-11 00:59:33 +0900

    create component directory & files

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,42 +1,9 @@
 import React, { Component } from 'react';
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
 import './App.css';
 // import logo from './logo.svg';
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    )
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    )
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    )
-  }
-}
 
 class App extends Component {
   render() {
